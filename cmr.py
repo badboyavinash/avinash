91304a41e04b00cf6d5fc539dc88c9e3d38dfba3
badboyavinash <35717757+badboyavinash@users.noreply.github.com>
2018-01-23 10:33:01 +0530

    Add files via upload

---
+++ cmr.py
@@ -0,0 +1,7 @@
+a=int(input("the value of a"))
+if a>0:
+    print("a is positive")
+if a<0:
+    print("a is negative")
+if a=0:
+    print("a is dhol")
---
+++ input.py
@@ -0,0 +1,5 @@
+a=input("enter a value")
+b=input("enter b value")
+c=int(a)%int(b)
+print(c)
+
